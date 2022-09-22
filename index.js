b2d4a9d0de28b6107f848b1023a5fd1ab36fa787
Maria Cristina Ordonez <crisordonez44@gmail.com>
2022-09-22 09:10:40 -0500

    commit tree fad724dfb4eab1d178c8e23dc346a94ebf38f2e7

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1= 31;
+const num2= 2;
+const multiply=  num1*num2
 
-// Write your code here
+
+const random =Number.isInteger(Math.random)+1;
+
+
+
+const num3= 17;
+const num4= 13;
+const mod = num3%num4
+
+
+let max=  Math.max(1,5,7,9,12,20);
+
